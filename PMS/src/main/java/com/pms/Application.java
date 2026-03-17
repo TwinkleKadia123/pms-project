ae8b49066764e7430f7a7364d3df0ffd6ffda618
ADMIN <ADMIN@DESKTOP-FV8CCHL.bbrouter>
2026-03-17 14:27:31 +0530

    Added loggers

--- PMS/src/main/java/com/pms/Application.java
+++ PMS/src/main/java/com/pms/Application.java
@@ -3,12 +3,18 @@ package com.pms;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
 @ComponentScan
 public class Application {
 	
+	private static final Logger logger = LoggerFactory.getLogger(Application.class);
+	
 	public static void main(String[] args) {
+		
+		 logger.info("Main Entry Point Class");
 		
 		SpringApplication.run(Application.class, args);
 	}
